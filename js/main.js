acba651ab895706a84299cc9d677a5eb52880960
Gabriel Suarez <lixez@live.com.ar>
2015-02-09 13:51:59 -0300

    Archivo de Inicialización

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+var existe_db
+var db
+
+
+
+function onLoad() {
+        document.addEventListener("deviceready", onDeviceReady, false);
+    };
+
+
+function onDeviceReady() {
+        
+	navigator.notification.alert("Esta Aplicación esta bien Configurada");
+}
